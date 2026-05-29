110f351a477d51f672a4907ae363b08ff1807863
Shappow <shadowfeugnet.tf@gmail.com>
2026-05-29 14:04:39 +0900

    Configurer le déploiement GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,10 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shappow.github.io',
+  base: '/Portfolio',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [react()]
 });
